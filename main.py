ab7f8a4fd9ba1b2ee306d3121aad4689832f13e9
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-01-28 20:31:31 +0100

    update main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from modules.students import add_student, list_students
+from modules.grades import add_grade, view_grades
 
 def main_menu():
     while True:
         print("\n=== School Menu ===")
         print("1. Add Student")
         print("2. List Students")
-        print("3. Exit")
+        print("3. Add Grades")
+        print("4. View Grades")
+        print("5. Exit")
 
         choice = input("Enter your choice: ")
 
@@ -14,6 +17,10 @@ def main_menu():
         elif choice == "2":
             list_students()
         elif choice == "3":
+            add_grade()
+        elif choice == "4":
+            view_grades()
+        elif choice == "5":
             print("Exiting the program. Goodbye!")
             break
         else:
